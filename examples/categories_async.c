08852f5320448ea0672877b34081b89a7ce7b608
Roberto Guido <bob4job@gmail.com>
2012-01-22 01:15:13 +0100

    updated link of the website for the project updated copyright note to 2012

--- examples/categories_async.c
+++ examples/categories_async.c
@@ -1,5 +1,5 @@
 /*  libopengdesktop
- *  Copyright (C) 2009/2011 Roberto -MadBob- Guido <bob4job@gmail.com>
+ *  Copyright (C) 2009/2012 Roberto -MadBob- Guido <bob4job@gmail.com>
  *
  *  This is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
